71a268a495cc75c8d7689142f4824bb1e1760d40
arnauld albert <arnauld.albert@uha.fr>
2024-04-17 12:07:23 +0200

    creation d'une seconde application de biblio

--- biblioproject/biblioproject/settings.py
+++ biblioproject/biblioproject/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bibliotheque.apps.BibliothequeConfig',
+    'myfirstapp.apps.MyfirstappConfig',
+    'biblio2.apps.Biblio2Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
